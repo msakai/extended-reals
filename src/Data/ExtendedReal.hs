dffd0a8d1a752bf3d2ab609cf2843e4e23faedd7
Masahiro Sakai <masahiro.sakai@gmail.com>
2014-09-01 21:40:29 +0900

    initial import

---
+++ src/Data/ExtendedReal.hs
@@ -0,0 +1,130 @@
+{-# OPTIONS_GHC -Wall #-}
+{-# LANGUAGE DeriveDataTypeable #-}
+-----------------------------------------------------------------------------
+-- |
+-- Module      :  Data.ExtendedReal
+-- Copyright   :  (c) Masahiro Sakai 2014
+-- License     :  BSD-style
+-- 
+-- Maintainer  :  masahiro.sakai@gmail.com
+-- Stability   :  provisional
+-- Portability :  non-portable (DeriveDataTypeable)
+--
+-- Extension of real numbers with positive/negative infinities (±∞).
+-- It is useful for describing various limiting behaviors in mathematics.
+--
+-- Remarks:
+--
+-- * @∞ - ∞@ is left undefined as usual,
+--   but we define @0 × ∞ = 0 × -∞ = 0@ by following the convention of
+--   probability or measure theory.
+--
+-- References:
+--
+-- * Wikipedia contributors, "Extended real number line," Wikipedia,
+--   The Free Encyclopedia, https://en.wikipedia.org/wiki/Extended_real_number_line
+--   (accessed September 1, 2014).
+--
+-----------------------------------------------------------------------------
+module Data.ExtendedReal
+  ( Extended (..)
+  , isFinite
+  , isInfinite
+  ) where
+
+import Prelude hiding (isInfinite)
+import Control.DeepSeq
+import Data.Data
+import Data.Hashable
+import Data.Typeable
+
+-- | @Extended r@ is an extension of /r/ with positive/negative infinity (±∞).
+data Extended r
+  = NegInf    -- ^ negative infinity (-∞)
+  | Finite !r -- ^ finite value
+  | PosInf    -- ^ positive infinity (+∞)
+  deriving (Ord, Eq, Show, Read, Typeable, Data)
+
+instance Bounded (Extended r) where
+  minBound = NegInf
+  maxBound = PosInf
+
+instance Functor Extended where
+  fmap _ NegInf = NegInf
+  fmap f (Finite x) = Finite (f x)
+  fmap _ PosInf = PosInf
+
+instance NFData r => NFData (Extended r) where
+  rnf (Finite x) = rnf x
+  rnf _ = ()
+
+instance Hashable r => Hashable (Extended r) where
+  hashWithSalt s NegInf     = s `hashWithSalt` (0::Int)
+  hashWithSalt s (Finite x) = s `hashWithSalt` (1::Int) `hashWithSalt` x
+  hashWithSalt s PosInf     = s `hashWithSalt` (2::Int)
+
+-- | @isFinite x = not (isInfinite x)@.
+isFinite :: Extended r -> Bool
+isFinite (Finite _) = True
+isFinite _ = False
+
+-- | @isInfinite x@ returns @True@ iff @x@ is @PosInf@ or @NegInf@.
+isInfinite :: Extended r -> Bool
+isInfinite = not . isFinite
+
+-- | Note that @Extended r@ is /not/ a field, nor a ring.
+-- 
+-- @PosInf + NegInf@ is left undefined as usual,
+-- but we define @0 * PosInf = 0 * NegInf = 0@ by following the convention of probability or measure theory.
+instance (Num r, Ord r) => Num (Extended r) where
+  Finite a + Finite b = Finite (a+b)
+  PosInf + NegInf = error "PosInf + NegInf is undefined"
+  NegInf + PosInf = error "NegInf + PosInf is undefined"
+  PosInf + _ = PosInf
+  _ + PosInf = PosInf
+  NegInf + _ = NegInf
+  _ + NegInf = NegInf
+
+  Finite x1 * e = scale x1 e
+  e * Finite x2 = scale x2 e
+  PosInf * PosInf = PosInf
+  PosInf * NegInf = NegInf
+  NegInf * PosInf = NegInf
+  NegInf * NegInf = PosInf
+
+  negate NegInf = PosInf
+  negate (Finite x) = Finite (negate x)
+  negate PosInf = NegInf
+
+  abs NegInf = PosInf
+  abs (Finite x) = Finite (abs x)
+  abs PosInf = PosInf
+
+  signum NegInf = Finite (-1)
+  signum (Finite x) = Finite (signum x)
+  signum PosInf = Finite 1
+
+  fromInteger = Finite . fromInteger  
+
+-- | Note that @Extended r@ is /not/ a field, nor a ring.
+instance (Fractional r, Ord r) => Fractional (Extended r) where
+  recip (Finite x) = Finite (1/x)
+  recip _ = Finite 0
+
+  fromRational = Finite . fromRational
+
+-- Note that we define @0 * PosInf = 0 * NegInf = 0@ by the convention of probability or measure theory.
+scale :: (Num r, Ord r) => r -> Extended r -> Extended r
+scale a e = seq e $
+  case a `compare` 0 of
+    EQ -> Finite 0
+    GT ->
+      case e of
+        NegInf   -> NegInf
+        Finite b -> Finite (a*b)
+        PosInf   -> PosInf
+    LT ->
+      case e of
+        NegInf   -> PosInf
+        Finite b -> Finite (a*b)
+        PosInf   -> NegInf
---
+++ test/TestExtendedReal.hs
@@ -0,0 +1,148 @@
+{-# OPTIONS_GHC -Wall #-}
+{-# LANGUAGE TemplateHaskell, ScopedTypeVariables #-}
+
+import Prelude hiding (isInfinite)
+import Control.Exception (SomeException, evaluate, try)
+import Control.Monad
+import Data.Maybe
+import System.IO.Unsafe (unsafePerformIO)
+import Test.HUnit hiding (Test)
+import Test.QuickCheck
+import Test.Framework.TH
+import Test.Framework.Providers.HUnit
+import Test.Framework.Providers.QuickCheck2
+
+import Data.ExtendedReal
+
+-- ----------------------------------------------------------------------
+
+instance Arbitrary r => Arbitrary (Extended r) where
+  arbitrary = 
+    oneof
+    [ return NegInf
+    , return PosInf
+    , liftM Finite arbitrary
+    ]
+
+eval :: a -> Maybe a
+eval a = unsafePerformIO $ do
+  ret <- try (evaluate a)
+  case ret of
+    Left (_::SomeException) -> return Nothing
+    Right b -> return $ Just b
+
+isDefined :: a -> Bool
+isDefined = isJust . eval
+
+-- ----------------------------------------------------------------------
+
+prop_add_comm :: Property
+prop_add_comm =
+  forAll arbitrary $ \(a :: Extended Rational) ->
+  forAll arbitrary $ \b ->
+    eval (a + b) == eval (b + a)
+
+prop_add_assoc :: Property
+prop_add_assoc =
+  forAll arbitrary $ \(a :: Extended Rational) ->
+  forAll arbitrary $ \b ->
+  forAll arbitrary $ \c ->
+    eval (a + (b + c)) == eval ((a + b) + c)
+
+prop_add_unit :: Property
+prop_add_unit =
+  forAll arbitrary $ \(a :: Extended Rational) ->
+    0 + a == a
+
+prop_add_monotone :: Property
+prop_add_monotone =
+  forAll arbitrary $ \(a :: Extended Rational) ->
+  forAll arbitrary $ \b ->
+  forAll arbitrary $ \c ->
+    a <= b && isDefined (a+c) && isDefined (b+c)
+    ==> a+c <= b+c
+
+prop_mult_comm :: Property
+prop_mult_comm =
+  forAll arbitrary $ \(a :: Extended Rational) ->
+  forAll arbitrary $ \b ->
+    a * b == b * a
+
+-- PosInf + NegInf is left undefined
+case_add_PosInf_NegInf :: IO ()
+case_add_PosInf_NegInf =
+  eval (PosInf + NegInf :: Extended Rational) @?= Nothing
+
+prop_mult_assoc :: Property
+prop_mult_assoc =
+  forAll arbitrary $ \(a :: Extended Rational) ->
+  forAll arbitrary $ \b ->
+  forAll arbitrary $ \c ->
+    a * (b * c) == (a * b) * c
+
+prop_mult_unit :: Property
+prop_mult_unit =
+  forAll arbitrary $ \(a :: Extended Rational) ->
+    1 * a == a
+
+prop_mult_dist :: Property
+prop_mult_dist =
+  forAll arbitrary $ \(a :: Extended Rational) ->
+  forAll arbitrary $ \b ->
+  forAll arbitrary $ \c ->
+    isDefined (a * (b + c)) && isDefined (a * b + a * c)
+    ==> eval (a * (b + c)) == eval (a * b + a * c)
+
+prop_mult_zero :: Property
+prop_mult_zero = 
+  forAll arbitrary $ \(a :: Extended Rational) ->
+    0 * a == 0
+
+prop_mult_monotone :: Property
+prop_mult_monotone =
+  forAll arbitrary $ \(a :: Extended Rational) ->
+  forAll arbitrary $ \b ->
+  forAll arbitrary $ \c ->
+    a <= b && c > 0 && isDefined (a*c) && isDefined (b*c)
+    ==> a*c <= b*c
+
+-- We define 0 * PosInf = 0
+case_mult_zero_PosInf :: IO ()
+case_mult_zero_PosInf =
+  0 * PosInf @?= (0 :: Extended Rational)
+
+-- We define 0 * NegInf = 0
+case_mult_zero_NegInf :: IO ()
+case_mult_zero_NegInf =
+  0 * NegInf @?= (0 :: Extended Rational)
+
+prop_negate_inverse :: Property
+prop_negate_inverse = 
+  forAll arbitrary $ \(a :: Extended Rational) ->
+    negate (negate a) == a
+
+prop_signum_abs :: Property
+prop_signum_abs =
+  forAll arbitrary $ \(a :: Extended Rational) ->
+    signum a * abs a == a
+
+prop_recip_inverse :: Property
+prop_recip_inverse =
+  forAll arbitrary $ \(a :: Extended Rational) ->
+    isFinite a && a /= 0 ==> recip (recip a) == a
+
+prop_NegInf_smallest :: Property
+prop_NegInf_smallest =
+  forAll arbitrary $ \(a :: Extended Rational) ->
+    NegInf <= a
+
+prop_PosInf_largest :: Property
+prop_PosInf_largest =
+  forAll arbitrary $ \(a :: Extended Rational) ->
+    a <= PosInf
+
+-- ----------------------------------------------------------------------
+-- Test harness
+
+main :: IO ()
+main = $(defaultMainGenerator)
